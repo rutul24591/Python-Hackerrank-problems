7a769a99358cd7b1040451b43b213d3f76b6ba2b
rutul <rutul591@gmail.com>
2016-08-20 13:58:14 -0700

    arithmeticops

---
+++ python-arithmetic-operators.py
@@ -0,0 +1,10 @@
+# Enter your code here. Read input from STDIN. Print output to
+
+
+# We need to perform type conversation or it print out the concatenation of a & b and throw an error for a-b
+a = int(input())
+b = int(input())
+
+print(a + b)
+print(a - b)
+print(a * b)
