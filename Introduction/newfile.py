a1cce22b0467eff1359c608b0750734ecc60d7b7
rutul <rutul591@gmail.com>
2016-09-08 23:34:30 -0700

    random file not of hackerrank

---
+++ Introduction/newfile.py
@@ -0,0 +1,49 @@
+# x = input()
+# y = input()
+# print(type(x))
+# print(type(y))
+# print("The sum of ", x , " and " , y , " is ", x+y ,".", sep="")
+
+# person = input("Enter your name: ")
+# greeting = "Hello {}!".format(person)
+# print(greeting)
+
+#
+# x = 30
+# y = 20
+# sum = x+y
+# product = x*y
+# formatStr = "{x}+{y}= {sum}; {x}*{y}={product}"
+# #formatStr = "{0} + {1} = {2}; {0} * {1} ={3}."
+# equation= formatStr.format(**locals())
+# print(equation)
+
+# s= "missisippi"
+# s.find('e')
+
+#
+# numberFormat =" count in spanish: {one} {two} {three}"
+# withSubstitutions = numberFormat.format(one="uno", two="dos",three="tres")
+# print(withSubstitutions)
+
+# def createDictionary():
+#     #Returns a tiny Spanish dictionary
+#     spanish = dict() # creates an empty dictionary
+#     spanish[’hello’] = ’hola’
+#     spanish[’yes’] = ’si’
+#     spanish[’one’] = ’uno’
+#     spanish[’two’] = ’dos’
+#     spanish[’three’] = ’tres’
+#     spanish[’red’] = ’rojo’
+#     spanish[’black’] = ’negro’
+#     spanish[’green’] = ’verde’
+#     spanish[’blue’] = ’azul’
+#     return spanish
+#
+# def main():
+#     dictionary = createDictionary()
+#     print(dictionary[’two’])
+#     print(dictionary[’red’])
+#
+#
+# main()
